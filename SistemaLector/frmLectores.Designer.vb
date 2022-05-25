538dbb083adc3718d4ec7005296e2c7cc1a2485e
davidjuarezr <djuarezr@gmail.com>
2022-05-25 00:37:33 -0500

    Se inserto un windowform que va ser un formulario, se le agrego codigo para que se pueda mover y la parte de busqueda y el icono de agregar

---
+++ SistemaLector/frmLectores.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmLectores
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLectores))
+        Me.panelTitulo = New System.Windows.Forms.Panel()
+        Me.btnCerrar = New System.Windows.Forms.Button()
+        Me.lblTitulo = New System.Windows.Forms.Label()
+        Me.panelBusqueda = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtBuscar = New System.Windows.Forms.TextBox()
+        Me.panelContenido = New System.Windows.Forms.Panel()
+        Me.panelLateral = New System.Windows.Forms.Panel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.panelTitulo.SuspendLayout()
+        Me.panelBusqueda.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.panelContenido.SuspendLayout()
+        Me.panelLateral.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'panelTitulo
+        '
+        Me.panelTitulo.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(204, Byte), Integer), CType(CType(1, Byte), Integer))
+        Me.panelTitulo.Controls.Add(Me.btnCerrar)
+        Me.panelTitulo.Controls.Add(Me.lblTitulo)
+        Me.panelTitulo.Dock = System.Windows.Forms.DockStyle.Top
+        Me.panelTitulo.Location = New System.Drawing.Point(0, 0)
+        Me.panelTitulo.Name = "panelTitulo"
+        Me.panelTitulo.Size = New System.Drawing.Size(614, 54)
+        Me.panelTitulo.TabIndex = 0
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCerrar.FlatAppearance.BorderSize = 0
+        Me.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCerrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnCerrar.Location = New System.Drawing.Point(567, 2)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(47, 51)
+        Me.btnCerrar.TabIndex = 1
+        Me.btnCerrar.Text = "X"
+        Me.btnCerrar.UseVisualStyleBackColor = True
+        '
+        'lblTitulo
+        '
+        Me.lblTitulo.AutoSize = True
+        Me.lblTitulo.Font = New System.Drawing.Font("Cambria", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblTitulo.Location = New System.Drawing.Point(3, 16)
+        Me.lblTitulo.Name = "lblTitulo"
+        Me.lblTitulo.Size = New System.Drawing.Size(100, 22)
+        Me.lblTitulo.TabIndex = 0
+        Me.lblTitulo.Text = "LECTORES"
+        '
+        'panelBusqueda
+        '
+        Me.panelBusqueda.BackColor = System.Drawing.Color.White
+        Me.panelBusqueda.Controls.Add(Me.txtBuscar)
+        Me.panelBusqueda.Controls.Add(Me.PictureBox1)
+        Me.panelBusqueda.Dock = System.Windows.Forms.DockStyle.Top
+        Me.panelBusqueda.Location = New System.Drawing.Point(0, 54)
+        Me.panelBusqueda.Name = "panelBusqueda"
+        Me.panelBusqueda.Size = New System.Drawing.Size(614, 48)
+        Me.panelBusqueda.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(3, -2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(48, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtBuscar.Location = New System.Drawing.Point(61, 11)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(506, 26)
+        Me.txtBuscar.TabIndex = 3
+        '
+        'panelContenido
+        '
+        Me.panelContenido.BackColor = System.Drawing.Color.White
+        Me.panelContenido.Controls.Add(Me.panelLateral)
+        Me.panelContenido.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.panelContenido.Location = New System.Drawing.Point(0, 102)
+        Me.panelContenido.Name = "panelContenido"
+        Me.panelContenido.Size = New System.Drawing.Size(614, 349)
+        Me.panelContenido.TabIndex = 2
+        '
+        'panelLateral
+        '
+        Me.panelLateral.Controls.Add(Me.PictureBox2)
+        Me.panelLateral.Dock = System.Windows.Forms.DockStyle.Right
+        Me.panelLateral.Location = New System.Drawing.Point(500, 0)
+        Me.panelLateral.Name = "panelLateral"
+        Me.panelLateral.Size = New System.Drawing.Size(114, 349)
+        Me.panelLateral.TabIndex = 0
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(0, 81)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(114, 114)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 0
+        Me.PictureBox2.TabStop = False
+        '
+        'frmLectores
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(614, 451)
+        Me.Controls.Add(Me.panelContenido)
+        Me.Controls.Add(Me.panelBusqueda)
+        Me.Controls.Add(Me.panelTitulo)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmLectores"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "frmLectores"
+        Me.panelTitulo.ResumeLayout(False)
+        Me.panelTitulo.PerformLayout()
+        Me.panelBusqueda.ResumeLayout(False)
+        Me.panelBusqueda.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.panelContenido.ResumeLayout(False)
+        Me.panelLateral.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents panelTitulo As Panel
+    Friend WithEvents lblTitulo As Label
+    Friend WithEvents btnCerrar As Button
+    Friend WithEvents panelBusqueda As Panel
+    Friend WithEvents txtBuscar As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents panelContenido As Panel
+    Friend WithEvents panelLateral As Panel
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ SistemaLector/frmLectores.vb
@@ -0,0 +1,25 @@
+﻿Public Class frmLectores
+    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
+        Close()
+    End Sub
+
+    Dim ex, ey As Integer
+    Dim Arrastre As Boolean
+
+    Private Sub panelTitulo_MouseDown(sender As Object, e As MouseEventArgs) Handles panelTitulo.MouseDown
+        ex = e.X
+        ey = e.Y
+        Arrastre = True
+    End Sub
+
+    Private Sub panelTitulo_MouseMove(sender As Object, e As MouseEventArgs) Handles panelTitulo.MouseMove
+        If Arrastre Then Me.Location = Me.PointToScreen(New Point(frmLectores.MousePosition.X - Me.Location.X - ex, frmLectores.MousePosition.Y - Me.Location.Y - ey))
+    End Sub
+
+    Private Sub panelTitulo_MouseUp(sender As Object, e As MouseEventArgs) Handles panelTitulo.MouseUp
+        Arrastre = False
+    End Sub
+    '<a href = "https://www.flaticon.es/iconos-gratis/buscar" title="buscar iconos">Buscar iconos creados por inkubators - Flaticon</a>
+    '<a href="https://www.flaticon.es/iconos-gratis/mas" title="más iconos">Más iconos creados por Smashicons - Flaticon</a>
+    '<a href="https://www.flaticon.es/iconos-gratis/mas" title="mas iconos">Mas iconos creados por Pixel Buddha - Flaticon</a>
+End Class
