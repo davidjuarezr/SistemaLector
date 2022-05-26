10993468c7d13316cc7a93840e87d541f1c3be0e
davidjuarezr <djuarezr@gmail.com>
2022-05-26 17:28:12 -0500

    Se agrega el código para  modificar y actualizar registros

--- SistemaLector/frmLectores.vb
+++ SistemaLector/frmLectores.vb
@@ -2,6 +2,10 @@
 
 Public Class frmLectores
 
+    Private Sub frmLectores_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Mostrar()
+    End Sub
+
     Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
         Close()
     End Sub
@@ -107,9 +111,91 @@ Public Class frmLectores
         End Try
     End Sub
 
-    Private Sub frmLectores_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Mostrar()
+    Private Sub Buscar()
+        Dim dt As New DataTable
+        Dim da As New SqlDataAdapter
+
+        Try
+            Abrir_conexion()
+            da = New SqlDataAdapter("Buscar_Lector", conexion)
+            da.SelectCommand.CommandType = 4
+            da.SelectCommand.Parameters.AddWithValue("@Busqueda", txtBuscar.Text)
+            da.Fill(dt)
+            dataLectores.DataSource = dt
+            Cerrar_conexion()
+
+            'cambiar ancho de las columnas(encabezados)
+            dataLectores.Columns(0).Width = 180
+            dataLectores.Columns(1).Width = 250
+            dataLectores.Columns(2).Width = 100
+            dataLectores.Columns(3).Width = 300
+            dataLectores.Columns(4).Width = 300
+
+            'Cambiar apariencia de los encabezados
+            dataLectores.EnableHeadersVisualStyles = False
+            Dim estilo As DataGridViewCellStyle = New DataGridViewCellStyle()
+
+            estilo.BackColor = Color.White
+            estilo.ForeColor = Color.Black
+            estilo.Font = New Font("Segoe UI", 10, FontStyle.Regular Or FontStyle.Bold)
+            dataLectores.ColumnHeadersDefaultCellStyle = estilo
+        Catch ex As Exception
+
+        End Try
     End Sub
+
+    Private Sub dataLectores_CellContentDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dataLectores.CellContentDoubleClick
+        panelDatos.Visible = True
+
+        Try
+            txtIdentidad.Text = dataLectores.SelectedCells.Item(0).Value
+            txtNombre.Text = dataLectores.SelectedCells.Item(1).Value
+            txtTelefono.Text = dataLectores.SelectedCells.Item(2).Value
+            txtDireccion.Text = dataLectores.SelectedCells.Item(3).Value
+            txtObservaciones.Text = dataLectores.SelectedCells.Item(4).Value
+
+            btnGuardar.Enabled = False
+            btnModificar.Enabled = True
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        Dim cmd As New SqlCommand
+
+        If txtNombre.Text <> "" And txtIdentidad.Text <> "" Then
+            Try
+                Abrir_conexion()
+                cmd = New SqlCommand("Editar_Lector", conexion)
+                cmd.CommandType = 4
+
+                cmd.Parameters.AddWithValue("@IdLector", txtIdentidad.Text.ToString)
+                cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text.ToString)
+                cmd.Parameters.AddWithValue("@Telefono", txtTelefono.Text)
+                cmd.Parameters.AddWithValue("@Direccion", txtDireccion.Text.ToString)
+                cmd.Parameters.AddWithValue("@Observaciones", txtObservaciones.Text.ToString)
+                cmd.ExecuteNonQuery()
+
+                Cerrar_conexion()
+
+                panelDatos.Visible = False
+                Limpiar()
+                Mostrar()
+            Catch ex As Exception
+
+            End Try
+        Else
+            MsgBox("Los campos Numero de Identidad y Nombre, son obligatorios", vbInformation + vbOK, "Sistema Letores")
+        End If
+    End Sub
+
+    Private Sub txtBuscar_TextChanged(sender As Object, e As EventArgs) Handles txtBuscar.TextChanged
+        Buscar()
+        panelDatos.Visible = False
+    End Sub
+
 
     '<a href = "https://www.flaticon.es/iconos-gratis/buscar" title="buscar iconos">Buscar iconos creados por inkubators - Flaticon</a>
     '<a href="https://www.flaticon.es/iconos-gratis/mas" title="más iconos">Más iconos creados por Smashicons - Flaticon</a>
